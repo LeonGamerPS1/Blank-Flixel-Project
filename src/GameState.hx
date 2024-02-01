a3be4e9e5f2bf177e61a162feebc3e4843fcadf1
Rudyrue <73214127+Rudyrue@users.noreply.github.com>
2024-02-01 13:16:31 -0700

    ok github

---
+++ src/GameState.hx
@@ -0,0 +1,13 @@
+package;
+
+class GameState extends flixel.FlxState {
+	override public function create() { 
+		super.create();
+
+		final text = new flixel.text.FlxText(0, 0, 1000, 'Hello World!', 30);
+		text.active = false;
+		text.alignment = 'center';
+		text.screenCenter();
+		add(text);
+	}
+}
---
+++ src/Main.hx
@@ -0,0 +1,46 @@
+package;
+
+import flixel.FlxG;
+import flixel.FlxGame;
+import openfl.Lib;
+import openfl.display.Sprite;
+import openfl.display.DisplayObject;
+import openfl.display.StageScaleMode;
+import flixel.graphics.FlxGraphic;
+
+#if windows
+@:headerCode("
+	#include <windows.h>
+	#include <winuser.h>
+")
+#end
+
+class Main extends Sprite {
+	public function new() {
+		super();
+
+		addChild(new FlxGame(1280, 720, GameState, 120, true));
+		Lib.current.stage.scaleMode = StageScaleMode.NO_SCALE;
+		registerAsDPICompatible();
+		setFlxDefines(); 
+	}
+
+	function setFlxDefines() {
+		FlxG.mouse.visible = false;
+		FlxG.cameras.useBufferLocking = true;
+		FlxG.autoPause = false;
+		FlxG.fixedTimestep = false;
+		FlxG.mouse.useSystemCursor = true;
+		FlxGraphic.defaultPersist = false;
+	}
+
+	@:functionCode('
+        SetProcessDPIAware();
+    ')
+    public static function registerAsDPICompatible() {}
+
+	// Get rid of hit test function because mouse memory ramp up during first move (-Bolo)
+    @:noCompletion override function __hitTest(x:Float, y:Float, shapeFlag:Bool, stack:Array<DisplayObject>, interactiveOnly:Bool, hitObject:DisplayObject):Bool return false;
+    @:noCompletion override function __hitTestHitArea(x:Float, y:Float, shapeFlag:Bool, stack:Array<DisplayObject>, interactiveOnly:Bool, hitObject:DisplayObject):Bool return false;
+    @:noCompletion override function __hitTestMask(x:Float, y:Float):Bool return false;
+}
